4e45c638044df2cdba5fe374fb009d747131be80
anasansari01 <anasansari056789@gmail.com>
2025-02-17 22:28:44 +0530

    Complete the setup of frontend & backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import CreateBooks from './pages/CreateBooks'
+import ShowBook from './pages/ShowBook'
+import Home from './pages/Home'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
 
 const App = () => {
   return (
     <Routes>
-      <Route path='' element={}></Route>
-      <Route path='' element={}></Route>
-      <Route path='' element={}></Route>
-      <Route path='' element={}></Route>
-      <Route path='' element={}></Route>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/books/create' element={<CreateBooks/>}></Route>
+      <Route path='/books/edit/:id' element={<EditBook/>}></Route>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}></Route>
+      <Route path='/books/details/:id' element={<ShowBook/>}></Route>
     </Routes>
   )
 }
