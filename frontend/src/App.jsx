003b6488308c9f86ccb7e477eb5f5a0655ba15c1
anasansari01 <anasansari056789@gmail.com>
2025-02-19 23:14:27 +0530

    Completed MY BOOK STORE WEBPAGE using MERN with FrontEnd & BackEnd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import CreateBooks from './pages/CreateBooks'
-import ShowBook from './pages/ShowBook'
-import Home from './pages/Home'
-import EditBook from './pages/EditBook'
-import DeleteBook from './pages/DeleteBook'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import CreateBooks from "./pages/CreateBooks";
+import ShowBook from "./pages/ShowBook";
+import Home from "./pages/Home";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/books/create' element={<CreateBooks/>}/>
-      <Route path='/books/edit/:id' element={<EditBook/>}/>
-      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
-      <Route path='/books/details/:id' element={<ShowBook/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBooks />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
